54e5fd52961f57199dce491de470f26f97ca35fb
Zack Drescher <zackryandrescher@gmail.com>
2024-10-25 19:09:11 -0400

    add key settings to config

--- kalshi_tracker/config.py
+++ kalshi_tracker/config.py
@@ -1,15 +1,25 @@
 """Base settings for the project."""
 
+from __future__ import annotations
+
+from typing import TYPE_CHECKING, Self
+
+from pydantic import model_validator
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
-from kalshi_tracker.kalshi.kalshi_settings import KalshiLoginSettings
+if TYPE_CHECKING:
+    from kalshi_tracker.kalshi.kalshi_settings import (
+        KalshiKeySettings,
+        KalshiLoginSettings,
+    )
 
 
 class Settings(BaseSettings):
     """Base settings for the project."""
 
     # Kalshi API settings
-    kalshi: KalshiLoginSettings
+    kalshi_login: KalshiLoginSettings | None
+    kalshi_keys: KalshiKeySettings | None
 
     # loads config fron .env file
     model_config = SettingsConfigDict(
@@ -17,3 +27,12 @@ class Settings(BaseSettings):
         case_sensitive=False,
         env_file=".env",
     )
+
+    @model_validator(mode="after")
+    def validate_kalshi_settings(self) -> Self:
+        """Validate the Kalshi settings."""
+        if not self.kalshi_login and not self.kalshi_keys:
+            raise ValueError("Either kalshi_login or kalshi_keys must be provided.")
+        if self.kalshi_login and self.kalshi_keys:
+            raise ValueError("Only one of kalshi_login or kalshi_keys can be provided.")
+        return self
--- kalshi_tracker/kalshi/kalshi_settings.py
+++ kalshi_tracker/kalshi/kalshi_settings.py
@@ -1,18 +1,40 @@
 """Settings for the Kalshi API."""
 
+from __future__ import annotations
+
+from pathlib import Path
+
+from cryptography.hazmat.backends import default_backend
+from cryptography.hazmat.primitives import serialization
 from pydantic import BaseModel
 
 
 class KalshiLoginSettings(BaseModel):
-    """Settings for the Kalshi API."""
+    """Login Auth Settings for the Kalshi API."""
 
     email: str
     password: str
     host: str = "api.elections.kalshi.com/trade-api/v2"
 
-    class KalshiKeySettings(BaseModel):
-        """Settings for the Kalshi API."""
 
-        key: str
-        secret: str
-        host: str = "api.elections.kalshi.com/trade-api/v2"
+def load_private_key_from_file(file_path: str | Path) -> serialization.PrivateKeyTypes:
+    """Load a private key from a file."""
+
+    with Path(file_path).open("rb") as key_file:
+        return serialization.load_pem_private_key(
+            key_file.read(),
+            password=None,  # or provide a password if your key is encrypted
+            backend=default_backend(),
+        )
+
+
+class KalshiKeySettings(BaseModel):
+    """Key Based Auth Settings for the Kalshi API."""
+
+    key: str
+    secret_file: str
+    host: str = "api.elections.kalshi.com/trade-api/v2"
+
+    def __post_init__(self) -> None:
+        """Load the secret key from the file."""
+        self.secret = load_private_key_from_file(self.secret_file)
