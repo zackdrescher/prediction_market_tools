df89610f87614a3dc2df325468a552adde8bf379
Zack Drescher <zackryandrescher@gmail.com>
2024-10-25 19:08:03 -0400

    rename kalshi module to util

--- kalshi_tracker/kalshi/__init__.py
+++ kalshi_tracker/kalshi/__init__.py
@@ -1,6 +1,6 @@
 """Kalshi module for the Kalshi API."""
 
-from .kalshi import get_kalshi, get_kalshi_from_settings
 from .kalshi_settings import KalshiLoginSettings
+from .util import get_kalshi, get_kalshi_from_settings
 
 __all__ = ["get_kalshi", "get_kalshi_from_settings", "KalshiLoginSettings"]
