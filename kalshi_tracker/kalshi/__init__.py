9475418caa5c5ab67bfe81f1636b676049aee226
Zack Drescher <zackryandrescher@gmail.com>
2024-10-25 14:22:50 -0400

    actually fix imports

--- kalshi_tracker/kalshi/__init__.py
+++ kalshi_tracker/kalshi/__init__.py
@@ -1,5 +1,5 @@
 """Kalshi module for the Kalshi API."""
 
-from .kalshi_settings import KalshiSettings
+from .kalshi import get_kalshi, get_kalshi_from_settings
 
-__all__ = ["KalshiSettings"]
+__all__ = ["get_kalshi", "get_kalshi_from_settings"]
--- kalshi_tracker/kalshi/kalshi.py
+++ kalshi_tracker/kalshi/kalshi.py
@@ -2,13 +2,13 @@
 
 import kalshi_python
 
-from kalshi_tracker.config import Settings
-
 from .kalshi_settings import KalshiSettings
 
 
 def get_kalshi() -> kalshi_python.ApiInstance:
     """Get kalshi API key from environment vairables."""
+    from kalshi_tracker.config import Settings
+
     settings = Settings()
     return kalshi_python.ApiInstance(settings)
 
