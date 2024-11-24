577f9f82125b97f13ecf92c93e4b4e2abc65bbe5
Zack Drescher <zackryandrescher@gmail.com>
2024-11-24 15:31:17 -0500

    update docs

--- tests/test_api/test_get_markets.py
+++ tests/test_api/test_get_markets.py
@@ -1,10 +1,12 @@
+"""Test the get markets endpoint."""
+
 from fastapi.testclient import TestClient
 
 from .fixtures import client_fixture  # noqa: F401
 
 
 def test_get_markets__returns_data(client_fixture: TestClient) -> None:
-    """Test the get markets endpoint."""
+    """Test the get markets endpoint returns expect data."""
     response = client_fixture.get("/markets")
 
     assert response.status_code == 200
